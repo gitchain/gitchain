74fdd75ad681ba770c1d73ff7944fb525d6d2d84
Yurii Rashkovskii <yrashk@gmail.com>
2014-05-13 00:50:43 +0800

    Improve nonce finding implementation

--- block/block.go
+++ block/block.go
@@ -61,9 +61,12 @@ func (b *Block) FindNonce() {
 	target := targetFromBits(b.Bits)
 	i := big.NewInt(int64(0))
 	var n uint32
+	buf := bytes.NewBuffer([]byte{})
+	buf.Grow(192)
+	buf1 := bytes.NewBuffer([]byte{})
+	buf1.Grow(32)
 loop:
 	for n = 0; n < 4294967295; n++ {
-		buf := bytes.NewBuffer([]byte{})
 		binary.Write(buf, binary.LittleEndian, b.PreviousBlockHash)
 		binary.Write(buf, binary.LittleEndian, b.MerkleRootHash)
 		binary.Write(buf, binary.LittleEndian, b.Version)
@@ -71,12 +74,13 @@ loop:
 		binary.Write(buf, binary.LittleEndian, b.Bits)
 		binary.Write(buf, binary.LittleEndian, n) // current nonce
 		hash := fastsha256.Sum256(buf.Bytes())
-		buf1 := bytes.NewBuffer([]byte{})
-		binary.Write(buf1, binary.LittleEndian, hash)
+		binary.Write(buf1, binary.BigEndian, hash)
 		hash = fastsha256.Sum256(buf1.Bytes())
-		buf1 = bytes.NewBuffer([]byte{})
-		binary.Write(buf1, binary.LittleEndian, hash)
+		buf1.Reset()
+		binary.Write(buf1, binary.BigEndian, hash)
 		i.SetBytes(buf1.Bytes())
+		buf1.Reset()
+		buf.Reset()
 		if i.Cmp(target) == -1 {
 			b.Nonce = n
 			return
