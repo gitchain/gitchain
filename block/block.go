1be38c7935aa1fbbcc5d7a36f0b8e1b125df7993
Yurii Rashkovskii <yrashk@gmail.com>
2014-05-14 20:26:56 +0800

    Extract block mining functionality to a separate file

--- block/block.go
+++ block/block.go
@@ -1,9 +1,6 @@
 package block
 
 import (
-	"bytes"
-	"encoding/binary"
-	"math/big"
 	"time"
 
 	trans "../transaction"
@@ -55,38 +52,4 @@ func merkleRoot(data [][]byte) (types.Hash, error) {
 		return types.EmptyHash(), err
 	}
 	return types.NewHash(tree.Root().Hash), err
-}
-
-func (b *Block) FindNonce() {
-	target := targetFromBits(b.Bits)
-	i := big.NewInt(int64(0))
-	var n uint32
-	buf := bytes.NewBuffer([]byte{})
-	buf.Grow(192)
-	buf1 := bytes.NewBuffer([]byte{})
-	buf1.Grow(32)
-loop:
-	for n = 0; n < 4294967295; n++ {
-		binary.Write(buf, binary.LittleEndian, b.PreviousBlockHash)
-		binary.Write(buf, binary.LittleEndian, b.MerkleRootHash)
-		binary.Write(buf, binary.LittleEndian, b.Version)
-		binary.Write(buf, binary.LittleEndian, b.Timestamp)
-		binary.Write(buf, binary.LittleEndian, b.Bits)
-		binary.Write(buf, binary.LittleEndian, n) // current nonce
-		hash := fastsha256.Sum256(buf.Bytes())
-		binary.Write(buf1, binary.BigEndian, hash)
-		hash = fastsha256.Sum256(buf1.Bytes())
-		buf1.Reset()
-		binary.Write(buf1, binary.BigEndian, hash)
-		i.SetBytes(buf1.Bytes())
-		buf1.Reset()
-		buf.Reset()
-		if i.Cmp(target) == -1 {
-			b.Nonce = n
-			return
-		}
-	}
-	// Update timestamp and restart the process
-	b.Timestamp = time.Now().UTC().Unix()
-	goto loop
 }
