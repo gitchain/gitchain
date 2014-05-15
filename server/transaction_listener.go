6b2532dee2f6507512116642df845baa4673f87f
Yurii Rashkovskii <yrashk@gmail.com>
2014-05-15 15:25:04 +0800

    When mining, use the last block

--- server/transaction_listener.go
+++ server/transaction_listener.go
@@ -25,13 +25,18 @@ func listener() {
 	var blk *block.Block
 	blockChannel := make(chan *block.Block)
 	transactionsPool := make([]transaction.T, 0)
-	previousBlockHash := types.EmptyHash()
+	var previousBlockHash types.Hash
 
 loop:
 	select {
 	case msg = <-ch:
 		env.DB.PutTransaction(msg)
 		transactionsPool = append(transactionsPool, msg)
+		if blk, _ = env.DB.GetLastBlock(); blk == nil {
+			previousBlockHash = types.EmptyHash()
+		} else {
+			previousBlockHash = types.NewHash(blk.Hash())
+		}
 		blk = block.NewBlock(previousBlockHash, 0x1e00ffff, transactionsPool)
 		miningFactoryRequests <- MiningFactoryInstantiationRequest{Block: blk, ResponseChannel: blockChannel}
 	case newMinedBlock := <-blockChannel:
