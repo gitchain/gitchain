0b4ebc13cbc4d1e2a01f50eef40e097bb7c540af
Yurii Rashkovskii <yrashk@gmail.com>
2014-05-19 21:30:48 +0800

    Git object hashing

--- git/objects.go
+++ git/objects.go
@@ -1,6 +1,12 @@
 package git
 
+import (
+	"crypto/sha1"
+	"fmt"
+)
+
 type Object interface {
+	Hash() []byte
 }
 
 const (
@@ -16,14 +22,34 @@ type Commit struct {
 	Content []byte
 }
 
+func (o *Commit) Hash() []byte {
+	result := sha1.Sum(append(append([]byte(fmt.Sprintf("commit %d", len(o.Content))), 0), o.Content...))
+	return result[:]
+}
+
 type Tree struct {
 	Content []byte
+}
+
+func (o *Tree) Hash() []byte {
+	result := sha1.Sum(append(append([]byte(fmt.Sprintf("tree %d", len(o.Content))), 0), o.Content...))
+	return result[:]
 }
 
 type Blob struct {
 	Content []byte
 }
 
+func (o *Blob) Hash() []byte {
+	result := sha1.Sum(append(append([]byte(fmt.Sprintf("blob %d", len(o.Content))), 0), o.Content...))
+	return result[:]
+}
+
 type Tag struct {
 	Content []byte
+}
+
+func (o *Tag) Hash() []byte {
+	result := sha1.Sum(append(append([]byte(fmt.Sprintf("tag %d", len(o.Content))), 0), o.Content...))
+	return result[:]
 }
