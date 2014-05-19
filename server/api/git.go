6e292771ccaa55f0370e7b728570ba6b4351f809
Yurii Rashkovskii <yrashk@gmail.com>
2014-05-19 09:57:28 +0800

    Resume work on git http smart protocol

---
+++ server/api/git.go
@@ -0,0 +1,50 @@
+package api
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/bargez/pktline"
+	"github.com/gitchain/gitchain/repository"
+	"github.com/gorilla/mux"
+)
+
+func setupGitRoutes(r *mux.Router) {
+	// Git Server
+	r.Methods("POST").Path("/{path}/git-upload-pack").HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.WriteHeader(200)
+	})
+
+	r.Methods("POST").Path("/{path:.+}/git-receive-pack").HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.WriteHeader(200)
+	})
+
+	r.Methods("GET").Path("/{path:.+}/info/refs").HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		req.ParseForm()
+		service := req.Form["service"][0]
+
+		path := mux.Vars(req)["path"]
+		repo, err := srv.DB.GetRepository(path)
+		if err != nil {
+			log.Printf("git http protocol: error while retrieving repository %s: %v", path, err)
+			resp.WriteHeader(500)
+			return
+		}
+		if repo == nil || repo.Status == repository.PENDING {
+			resp.WriteHeader(404)
+			return
+		}
+		resp.Header().Add("Content-Type", fmt.Sprintf("application/x-%s-advertisement", service))
+		resp.Header().Add("Cache-Control", "no-cache")
+		enc := pktline.NewEncoder(resp)
+		enc.Encode([]byte(fmt.Sprintf("# service=%s\n", service)))
+		enc.Encode(nil)
+		enc.Encode(append(append(append(append([]byte("0000000000000000000000000000000000000000"), 32), capabilities()...), []byte{0, 32, 10}...)))
+		enc.Encode(nil)
+	})
+}
+
+func capabilities() []byte {
+	return []byte("capabilities^{} report-status delete-refs side-band-64k quiet ofs-delta agent=gitchain")
+}
--- server/api/http.go
+++ server/api/http.go
@@ -2,13 +2,11 @@ package api
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"mime"
 	"net/http"
 	"path/filepath"
 
-	"github.com/gitchain/gitchain/repository"
 	"github.com/gitchain/gitchain/server"
 	"github.com/gitchain/gitchain/ui"
 	"github.com/gorilla/mux"
@@ -25,36 +23,7 @@ func Start(srvr *server.T) {
 	r.Methods("POST").Path("/rpc").HandlerFunc(jsonRpcService().ServeHTTP)
 	r.Methods("GET").Path("/info").HandlerFunc(info)
 
-	// Git Server
-	r.Methods("POST").Path("/{path}/git-upload-pack").HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-		resp.Write([]byte(mux.Vars(req)["path"]))
-	})
-
-	r.Methods("POST").Path("/{path:.+}/git-receive-pack").HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-		b, _ := ioutil.ReadAll(req.Body)
-		log.Printf("%+v %s", req, b)
-
-		resp.Write([]byte(mux.Vars(req)["path"]))
-	})
-
-	r.Methods("GET").Path("/{path:.+}/info/refs").HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
-		path := mux.Vars(req)["path"]
-		repo, err := srv.DB.GetRepository(path)
-		if err != nil {
-			log.Printf("git http protocol: error while retrieving repository %s: %v", path, err)
-			resp.WriteHeader(500)
-			return
-		}
-		if repo == nil || repo.Status == repository.PENDING {
-			resp.WriteHeader(404)
-			return
-		}
-		b, _ := ioutil.ReadAll(req.Body)
-		log.Printf("%+v %s", req, b)
-		resp.WriteHeader(200)
-		resp.Write([]byte("00860000000000000000000000000000000000000000 capabilities^{} report-status delete-refs side-band-64k quiet ofs-delta agent=git/1.9.3"))
-		resp.Write([]byte("0000"))
-	})
+	setupGitRoutes(r)
 
 	// UI
 	r.Methods("GET").Path("/websocket").HandlerFunc(websocketHandler)
